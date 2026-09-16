ff1c8f18372ca621f76eb0a945da62f1f4ebac08
Rahulchandnani1 <64478378+Rahulchandnani1@users.noreply.github.com>
2026-09-16 16:49:29 +0530

    Update courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,26 +1,46 @@
 const express = require("express");
+
 const router = express.Router();
 
 const courseController = require("../controller/courseController");
+
 const authMiddleware = require("../middleware/authMiddleware");
-const upload = require("../middleware/uploadMiddleware");
 
 // Create Course
-router.post("/", authMiddleware, upload.single("thumbnail"), courseController.createCourse);
+router.post(
+  "/",
+  authMiddleware,
+  courseController.createCourse
+);
 
 // Get All Courses
-router.get("/", courseController.getCourses);
+router.get(
+  "/",
+  courseController.getCourses
+);
 
-// // Get Course By Id
- router.get("/:id", authMiddleware,courseController.getCourseById);
+// Get Course By Id
+router.get(
+  "/:id",
+  authMiddleware,
+  courseController.getCourseById
+);
 
 // Update Course
-router.put("/:id", authMiddleware, upload.single("thumbnail"), courseController.updateCourse);
+router.put(
+  "/:id",
+  authMiddleware,
+  courseController.updateCourse
+);
 
 // Delete Course
-router.delete("/:id", authMiddleware, courseController.deleteCourse);
+router.delete(
+  "/:id",
+  authMiddleware,
+  courseController.deleteCourse
+);
 
-// // Publish Course
+// Publish Course
 // router.patch("/:id/publish", authMiddleware, courseController.publishCourse);
 
 module.exports = router;
